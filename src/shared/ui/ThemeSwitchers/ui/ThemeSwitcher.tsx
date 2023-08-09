3a9aaa24c425cd3ad52ea4cede4b230dfdfc922a
valntyn <romanenko.valentyn1@gmail.com>
2023-08-09 18:00:27 +0300

    add reusable button in shared

--- src/shared/ui/ThemeSwitchers/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitchers/ui/ThemeSwitcher.tsx
@@ -1,6 +1,9 @@
-import {useTheme} from 'app/providers/ThemeProvider';
+import {Theme, useTheme} from 'app/providers/ThemeProvider';
 import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './ThemeSwitcher.module.scss';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import {Button, ThemeButton} from 'shared/ui/Button/Button';
 
 interface PropsThemeSwitcher {
     className?: string;
@@ -9,14 +12,13 @@ interface PropsThemeSwitcher {
 export const ThemeSwitcher = ({className}: PropsThemeSwitcher) => {
     const {theme, toggleTheme} = useTheme();
 
-    console.log(className);
-
     return (
-        <button
+        <Button
+            theme={ThemeButton.CLEAR}
             className={classNames(cls.themeSwitcher, {}, [className])}
             onClick={toggleTheme}
         >
-            TOGGLE
-        </button>
+            {theme !== Theme.LIGHT ? <DarkIcon /> : <LightIcon />}
+        </Button>
     );
 };
