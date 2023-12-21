57bc378c4484f6d8942d6a427d7fc04b7f48d3fd
valntyn <romanenko.valentyn1@gmail.com>
2023-12-21 20:23:28 +0200

    delete timeout, prepare for prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
 import { BuildOptions } from './types/config';
 
@@ -22,6 +23,11 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
         }),
     ];
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -10,6 +10,8 @@ export default (env: BuildEnv) => {
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
+        locales: path.resolve(__dirname, 'public', 'locales'),
     };
 
     const mode = env.mode || 'development';
