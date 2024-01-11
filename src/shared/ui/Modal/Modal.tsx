fa5f2fe8def9759ed94cd74b20023487a9be9fcf
valntyn <romanenko.valentyn1@gmail.com>
2024-01-11 11:49:18 +0200

    small fix with draggable modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,10 +1,10 @@
-import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import React, { ReactNode } from 'react';
+import { createUseGesture, dragAction } from '@use-gesture/react';
+import { animated, useSpring } from '@react-spring/web';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import { useTheme } from '@/app/providers/ThemeProvider';
 
 import { useModal } from '@/shared/lib/hooks/useModal';
-import { createUseGesture, dragAction } from '@use-gesture/react';
-import { animated, useSpring } from '@react-spring/web';
 import { Text, TextAlign, TextTheme } from '@/shared/ui/Text/Text';
 import { Overlay } from '../Overlay/Overlay';
 import cls from './Modal.module.scss';
@@ -56,12 +56,7 @@ export const Modal = ({
             target: ref,
             drag: {
                 from: () => [style.x.get(), style.y.get()],
-                bounds: {
-                    left: -window.innerWidth / 2,
-                    right: window.innerWidth / 2,
-                    top: -window.innerHeight / 2,
-                    bottom: window.innerHeight / 2,
-                },
+                bounds: document.body,
             },
             rubberband: true,
         },
@@ -78,7 +73,9 @@ export const Modal = ({
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
+            <div
+                className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}
+            >
                 <Overlay onClick={close} />
                 <animated.div
                     className={cls.wrapper}
