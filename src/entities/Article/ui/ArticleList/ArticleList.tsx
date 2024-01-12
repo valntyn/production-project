8a7f9cc5f9fcd39dfebb61920f8a11f7f2259ad3
valntyn <romanenko.valentyn1@gmail.com>
2024-01-12 12:21:35 +0200

    remove deprecated lib for virtualization

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,7 +1,5 @@
 import { HTMLAttributeAnchorTarget, memo } from 'react';
-import { List, ListRowProps, WindowScroller } from 'react-virtualized';
 import { Text } from '@/shared/ui/Text/Text';
-import { PAGE_ID } from '@/widgets/Page/Page';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
 import { Article, ArticleView } from '../../model/types/article';
@@ -21,7 +19,6 @@ interface ArticleListProps {
     isLoading?: boolean;
     view?: ArticleView;
     target?: HTMLAttributeAnchorTarget;
-    virtualized?: boolean;
 }
 
 export const ArticleList = memo((props: ArticleListProps) => {
@@ -31,11 +28,8 @@ export const ArticleList = memo((props: ArticleListProps) => {
         view = ArticleView.SMALL,
         isLoading,
         target,
-        virtualized = true,
     } = props;
     const isLarge = view === ArticleView.LARGE;
-    const itemsPerRow = isLarge ? 1 : 5;
-    const rowCount = isLarge ? articles.length : Math.ceil(articles.length / itemsPerRow);
 
     if (!isLoading && !articles.length) {
         return (
@@ -43,78 +37,20 @@ export const ArticleList = memo((props: ArticleListProps) => {
         );
     }
 
-    const rowRender = ({
-        index, key, style,
-    }: ListRowProps) => {
-        const items = [];
-        const fromIndex = index * itemsPerRow;
-        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
-
-        for (let i = fromIndex; i < toIndex; i += 1) {
-            items.push(
+    return (
+        <div
+            className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+        >
+            {articles.map((item) => (
                 <ArticleListItem
-                    article={articles[index]}
+                    article={item}
                     view={view}
                     className={cls.card}
                     target={target}
-                    key={articles[index].id}
-                />,
-            );
-        }
-
-        return (
-            <div
-                key={key}
-                style={style}
-                className={cls.row}
-            >
-                {items}
-            </div>
-        );
-    };
-
-    return (
-        <WindowScroller
-            scrollElement={document.getElementById(PAGE_ID) as Element}
-        >
-            {({
-                height,
-                width,
-                registerChild,
-                isScrolling,
-                onChildScroll,
-                scrollTop,
-            }) => (
-                <div
-                    className={classNames(cls.ArticleList, {}, [className, cls[view]])}
-                    // @ts-ignore
-                    ref={registerChild}
-                >
-                    {virtualized ? (
-                        <List
-                            height={height ?? 700}
-                            width={width ? width - 80 : 700}
-                            rowCount={rowCount}
-                            rowHeight={isLarge ? 700 : 330}
-                            rowRenderer={rowRender}
-                            autoHeight
-                            onScroll={onChildScroll}
-                            isScrolling={isScrolling}
-                            scrollTop={scrollTop}
-                        />
-                    ) : (articles.map((item) => (
-                        <ArticleListItem
-                            article={item}
-                            view={view}
-                            className={cls.card}
-                            target={target}
-                            key={item.id}
-                        />
-                    )))}
-
-                    {isLoading && getSkeletons(view)}
-                </div>
-            )}
-        </WindowScroller>
+                    key={item.id}
+                />
+            ))}
+            {isLoading && getSkeletons(view)}
+        </div>
     );
 });
--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -28,7 +28,6 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
             <ArticleList
                 articles={articles}
                 target="_blank"
-                virtualized={false}
             />
         </VStack>
     );
